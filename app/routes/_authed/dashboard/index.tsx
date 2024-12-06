55ab6d06bd858a75d202e9d020af683236163560
akinloluwami <akinkunmioye42@gmail.com>
2024-12-06 09:58:58 +0100

    render bookmark cards

--- app/routes/_authed/dashboard/index.tsx
+++ app/routes/_authed/dashboard/index.tsx
@@ -1,11 +1,24 @@
 import { createFileRoute } from "@tanstack/react-router";
+import BookmarkCard from "~/components/BookmarkCard";
 import NoLinks from "~/components/NoLinks";
+import { bookmarks } from "~/mock";
 
 export const Route = createFileRoute("/_authed/dashboard/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  const links = [];
-  return <div>{!links.length ? <NoLinks /> : <>here are the links</>}</div>;
+  return (
+    <div>
+      {!bookmarks.length ? (
+        <NoLinks />
+      ) : (
+        <div className="grid grid-cols-3 gap-4">
+          {bookmarks.map((bookmark, i) => (
+            <BookmarkCard key={i} {...bookmark} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
