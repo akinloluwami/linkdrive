f6a47c99c972a4bde946bb496fae5e04ddcbc00a
akinloluwami <akinkunmioye42@gmail.com>
2024-12-06 08:52:54 +0100

    calculate height for 90vh

--- app/components/NoLinks.tsx
+++ app/components/NoLinks.tsx
@@ -2,7 +2,7 @@ import { LinkCircle } from "solar-icon-set";
 
 const NoLinks = () => {
   return (
-    <div className="w-full h-[calc(100vh-60px)] flex items-center justify-center flex-col">
+    <div className="w-full h-[calc(90vh-60px)] flex items-center justify-center flex-col">
       <LinkCircle iconStyle="BoldDuotone" size={100} />
       <p className="font-medium text-2xl my-1">No links yet.</p>
       <p>Click "Add New" to add a new link or collection.</p>
