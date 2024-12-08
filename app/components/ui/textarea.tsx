ea7059d9ec2f919d9c69f0975d2e83930dbcbf7f
akinloluwami <akinkunmioye42@gmail.com>
2024-12-08 09:06:59 +0100

    create ui textarea

---
+++ app/components/ui/textarea.tsx
@@ -0,0 +1,40 @@
+import classNames from "classnames";
+import { AnimatePresence, motion } from "motion/react";
+import React, { TextareaHTMLAttributes } from "react";
+
+interface TextareaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
+  errorMessage?: string;
+}
+
+const Textarea: React.FC<TextareaProps> = ({
+  children,
+  errorMessage,
+  ...props
+}) => {
+  return (
+    <div className="">
+      <textarea
+        {...props}
+        className={classNames(
+          "text-accent font-medium bg-gray-200/70 outline-0 rounded-2xl py-3 w-full px-3 placeholder:text-accent/50 border-2 border-transparent focus:border-accent",
+          props.className
+        )}
+      />
+      <AnimatePresence>
+        {errorMessage && (
+          <motion.p
+            className="text-sm text-red-500"
+            initial={{ opacity: 0, height: 0 }}
+            animate={{ opacity: 1, height: 24 }}
+            exit={{ opacity: 0, height: 0 }}
+            transition={{ duration: 0.3 }}
+          >
+            {errorMessage}
+          </motion.p>
+        )}
+      </AnimatePresence>
+    </div>
+  );
+};
+
+export default Textarea;
