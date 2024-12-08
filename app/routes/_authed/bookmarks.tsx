df2a44b4b844b1e34111f396e59f4e5d9cb5bf1f
akinloluwami <akinkunmioye42@gmail.com>
2024-12-08 16:24:18 +0100

    open modal from bookmarks page

--- app/routes/_authed/bookmarks.tsx
+++ app/routes/_authed/bookmarks.tsx
@@ -1,18 +1,31 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { Plus } from "lucide-react";
 import BookmarkCard from "~/components/bookmark-card";
 import NoLinks from "~/components/empty-states/no-links";
+import Button from "~/components/ui/button";
 import { bookmarks } from "~/mock";
+import useGlobalModalStore from "~/stores/globalModalStore";
 
 export const Route = createFileRoute("/_authed/bookmarks")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const { setActiveModal } = useGlobalModalStore();
   return (
     <div>
-      <h1 className="text-2xl font-medium text-accent mb-3">
-        Welcome to LinkDrive
-      </h1>
+      <div className="flex items-center justify-between mt-6 mb-12">
+        <h1 className="text-2xl font-medium text-accent mb-3">
+          Welcome to LinkDrive
+        </h1>
+        <Button
+          className="!w-fit px-6"
+          onClick={() => setActiveModal("add-new-link")}
+        >
+          <Plus />
+          Add Bookmark
+        </Button>
+      </div>
       {!bookmarks.length ? (
         <NoLinks />
       ) : (
