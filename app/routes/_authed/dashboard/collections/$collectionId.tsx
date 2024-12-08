76e927caeb293a94078fff98c50038a372d3e7a0
akinloluwami <akinkunmioye42@gmail.com>
2024-12-08 12:49:07 +0100

    create collection page

---
+++ app/routes/_authed/dashboard/collections/$collectionId.tsx
@@ -0,0 +1,16 @@
+import { createFileRoute } from "@tanstack/react-router";
+import EmptyCollection from "~/components/empty-states/empty-collection";
+
+export const Route = createFileRoute(
+  "/_authed/dashboard/collections/$collectionId"
+)({
+  component: RouteComponent,
+});
+
+function RouteComponent() {
+  return (
+    <div>
+      <EmptyCollection />
+    </div>
+  );
+}
