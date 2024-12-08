4915e6ac1fa7a316b96622bdb2e876dcef2abbb0
akinloluwami <akinkunmioye42@gmail.com>
2024-12-08 12:48:28 +0100

    update styling

--- app/routes/_authed/dashboard.tsx
+++ app/routes/_authed/dashboard.tsx
@@ -10,15 +10,15 @@ function RouteComponent() {
   return (
     <>
       <GlobalModal />
-      <div className="flex">
+      <div className="flex relative">
         <div className="w-[30%] max-w-[270px]">
           <Sidebar />
         </div>
-        <div className="w-full">
-          <div className="bg-gray-100/20 w-full h-[60px] px-10 flex items-center justify-between sticky top-0 right-0">
+        <div className="w-full relative">
+          <div className="bg-gray-100 w-full h-[60px] px-10 flex items-center sticky top-0 right-0 inset-0">
             <div className=""></div>
           </div>
-          <div className="px-10 py-5 bg-gray-100/50">
+          <div className="px-10 py-5 bg-gray-100/50 min-h-[90vh]">
             <Outlet />
           </div>
         </div>
