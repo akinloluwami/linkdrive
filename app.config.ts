ebd689f6d9954264f289c347852d00e55db0a2d8
akinloluwami <akinkunmioye42@gmail.com>
2024-12-10 07:14:06 +0100

    downgrade tailwind version

--- app.config.ts
+++ app.config.ts
@@ -1,11 +1,9 @@
-import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "@tanstack/start/config";
 import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   vite: {
     plugins: [
-      // tailwindcss(),
       tsConfigPaths({
         projects: ["./tsconfig.json"],
       }),
