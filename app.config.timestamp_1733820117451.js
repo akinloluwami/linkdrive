41e653c0f84527b1cd679ef3718d5a20ab6822eb
akinloluwami <akinkunmioye42@gmail.com>
2024-12-10 09:42:08 +0100

    >>>>

--- app.config.timestamp_1733820117451.js
+++ app.config.timestamp_1733820117451.js
@@ -1,14 +1,17 @@
+// app.config.ts
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "@tanstack/start/config";
 import tsConfigPaths from "vite-tsconfig-paths";
-
-export default defineConfig({
+var app_config_default = defineConfig({
   vite: {
     plugins: [
       tailwindcss(),
       tsConfigPaths({
-        projects: ["./tsconfig.json"],
-      }),
-    ],
-  },
+        projects: ["./tsconfig.json"]
+      })
+    ]
+  }
 });
+export {
+  app_config_default as default
+};
