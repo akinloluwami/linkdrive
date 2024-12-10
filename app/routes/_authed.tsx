a62126a724c5e03e6bb74ff90c224261e1d579bc
akinloluwami <akinkunmioye42@gmail.com>
2024-12-10 04:41:16 +0100

    render bottom sheet component

--- app/routes/_authed.tsx
+++ app/routes/_authed.tsx
@@ -1,9 +1,9 @@
 import { createFileRoute, Outlet } from "@tanstack/react-router";
 import { createServerFn } from "@tanstack/start";
+import BottomTab from "~/components/bottom-tab";
 import GlobalModal from "~/components/global-modal";
 import GlobalSearch from "~/components/global-search";
 import Sidebar from "~/components/sidebar";
-import Input from "~/components/ui/input";
 import { useAppSession } from "~/utils/session";
 
 export const loginFn = createServerFn().handler(async () => {
@@ -27,32 +27,33 @@ export const loginFn = createServerFn().handler(async () => {
 });
 
 export const Route = createFileRoute("/_authed")({
-  loader: () => loginFn,
-  beforeLoad: ({ context }) => {
-    //@ts-ignore
-    if (!context.user) {
-      throw new Error("Not authenticated");
-    }
-  },
-  errorComponent: ({ error }) => {
-    if (error.message === "Not authenticated") {
-      return <div>Login</div>;
-    }
+  // loader: () => loginFn,
+  // beforeLoad: ({ context }) => {
+  //   //@ts-ignore
+  //   if (!context.user) {
+  //     throw new Error("Not authenticated");
+  //   }
+  // },
+  // errorComponent: ({ error }) => {
+  //   if (error.message === "Not authenticated") {
+  //     return <div>Login</div>;
+  //   }
 
-    throw error;
-  },
+  //   throw error;
+  // },
   component: () => {
     return (
       <>
         <GlobalModal />
+        <BottomTab />
         <div className="flex relative">
-          <div className="w-[30%] max-w-[270px]">
+          <div className="hidden lg:block w-[30%] max-w-[270px]">
             <Sidebar />
           </div>
           <div className="w-full relative">
-            <div className="bg-gray-100 w-full h-[60px] px-10 flex items-center justify-between sticky top-0 right-0 inset-0">
+            <div className="bg-gray-100 w-full h-[60px] lg:px-10 px-5 flex items-center justify-between sticky top-0 right-0 inset-0">
               <GlobalSearch />
-              <div className="size-12 rounded-full bg-accent/10 flex items-center justify-center">
+              <div className="size-12 rounded-full bg-accent/10 lg:flex items-center justify-center hidden">
                 <img
                   src="https://api.dicebear.com/9.x/adventurer/svg?seed=Luis"
                   alt="avatar"
@@ -60,7 +61,7 @@ export const Route = createFileRoute("/_authed")({
                 />
               </div>
             </div>
-            <div className="px-10 py-5 bg-gray-100/50 min-h-[90vh]">
+            <div className="lg:px-10 px-5 py-5 bg-gray-100/50 min-h-[90vh]">
               <Outlet />
             </div>
           </div>
