28ca133346b10af1842539bdb56b943d38a204fa
akinloluwami <akinkunmioye42@gmail.com>
2024-12-10 04:40:35 +0100

    tranform modal into a bottom sheet on mobile

--- app/components/ui/Modal.tsx
+++ app/components/ui/Modal.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { ArrowLeft, X as Close } from "lucide-react";
 import { AnimatePresence, motion } from "motion/react";
+import { isMobile } from "react-device-detect";
 
 interface ModalProps {
   title: string;
@@ -24,17 +25,35 @@ const Modal: React.FC<ModalProps> = ({
     <AnimatePresence>
       {isOpen && (
         <motion.div
-          className="fixed h-screen w-full flex items-center justify-center bg-accent/10 backdrop-blur-xs z-10 top-0 right-0 inset-0"
+          className="fixed h-screen w-full flex items-center justify-center bg-accent/10 backdrop-blur-xs z-20 top-0 right-0 inset-0"
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
+          exit={isMobile ? {} : { opacity: 0 }}
         >
           <motion.div
-            className="w-[500px] h-fit rounded-2xl bg-white shadow p-5"
-            initial={{ scale: 0.9, opacity: 0 }}
+            className="lg:w-[500px] w-full h-fit rounded-2xl bg-white shadow p-5 bottom-0 lg:bottom-auto right-0 lg:right-auto fixed lg:static"
+            initial={
+              isMobile
+                ? {
+                    bottom: -150,
+                  }
+                : { scale: 0.9, opacity: 0 }
+            }
             layout
-            animate={{ scale: 1, opacity: 1 }}
-            exit={{ scale: 0.9, opacity: 0 }}
+            animate={
+              isMobile
+                ? {
+                    bottom: 0,
+                  }
+                : { scale: 0.9, opacity: 1 }
+            }
+            exit={
+              isMobile
+                ? {
+                    bottom: -300,
+                  }
+                : { scale: 0.9, opacity: 0 }
+            }
             transition={{ layout: { duration: 0.3 } }}
           >
             <div className="flex items-center justify-between">
