cb591b12ef15c7b2a3e26f889e2cf5ddc4230a4b
akinloluwami <akinkunmioye42@gmail.com>
2024-12-10 04:41:57 +0100

    update styling for mobile

--- app/routes/_authed/bookmarks.tsx
+++ app/routes/_authed/bookmarks.tsx
@@ -5,6 +5,7 @@ import NoLinks from "~/components/empty-states/no-links";
 import Button from "~/components/ui/button";
 import { bookmarks } from "~/mock";
 import useGlobalModalStore from "~/stores/globalModalStore";
+import { isDesktop } from "react-device-detect";
 
 export const Route = createFileRoute("/_authed/bookmarks")({
   component: RouteComponent,
@@ -14,22 +15,24 @@ function RouteComponent() {
   const { setActiveModal } = useGlobalModalStore();
   return (
     <div>
-      <div className="flex items-center justify-between mt-6 mb-12">
-        <h1 className="text-2xl font-medium text-accent mb-3">
+      <div className="flex items-center justify-between lg:mt-6 mt-1 lg:mb-12 mb-6">
+        <h1 className="lg:text-2xl text-lg font-medium text-accent mb-3">
           Welcome to LinkDrive
         </h1>
-        <Button
-          className="!w-fit px-6"
-          onClick={() => setActiveModal("add-new-link")}
-        >
-          <Plus />
-          Add Bookmark
-        </Button>
+        {isDesktop && (
+          <Button
+            className="!w-fit px-6"
+            onClick={() => setActiveModal("add-new-link")}
+          >
+            <Plus />
+            Add Bookmark
+          </Button>
+        )}
       </div>
       {!bookmarks.length ? (
         <NoLinks />
       ) : (
-        <div className="grid grid-cols-3 gap-4">
+        <div className="grid lg:grid-cols-3 grid-cols-2 gap-4">
           {bookmarks.map((bookmark, i) => (
             <BookmarkCard key={i} {...bookmark} />
           ))}
