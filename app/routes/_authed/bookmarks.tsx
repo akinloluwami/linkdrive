0bd68b1c02dc6ad45e542f8497fd1d4b0e40a998
akinloluwami <akinkunmioye42@gmail.com>
2024-12-16 07:29:30 +0100

    move code to separate component

--- app/routes/_authed/bookmarks.tsx
+++ app/routes/_authed/bookmarks.tsx
@@ -1,43 +1,10 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { Plus } from "lucide-react";
-import BookmarkCard from "@/components/bookmark-card";
-import NoLinks from "@/components/empty-states/no-links";
-import Button from "@/components/ui/button";
-import { bookmarks } from "@/mock";
-import useGlobalModalStore from "@/stores/globalModalStore";
-import { isDesktop } from "react-device-detect";
+import Bookmarks from "@/components/pages/bookmarks";
 
 export const Route = createFileRoute("/_authed/bookmarks")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  const { setActiveModal } = useGlobalModalStore();
-  return (
-    <div>
-      <div className="flex items-center justify-between lg:mt-6 mt-1 lg:mb-12 mb-6">
-        <h1 className="lg:text-2xl text-lg font-medium text-accent mb-3">
-          Welcome to LinkDrive
-        </h1>
-        {isDesktop && (
-          <Button
-            className="!w-fit px-6"
-            onClick={() => setActiveModal("add-new-link")}
-          >
-            <Plus />
-            Add Bookmark
-          </Button>
-        )}
-      </div>
-      {!bookmarks.length ? (
-        <NoLinks />
-      ) : (
-        <div className="grid lg:grid-cols-3 grid-cols-2 gap-4">
-          {bookmarks.map((bookmark, i) => (
-            <BookmarkCard key={i} {...bookmark} />
-          ))}
-        </div>
-      )}
-    </div>
-  );
+  return <Bookmarks />;
 }
